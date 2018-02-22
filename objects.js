4b1d9b074947b58f0e93c929b61cb79abb4e6943
yossi katzman <issoyk@gmail.com>
2018-02-22 13:05:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist: 'song'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return 
+  return Object.assign(playlist, {}, playlist, {[artistName]: song})
 } 
 
 function removeFromPlaylist(playlist, artistName) {
