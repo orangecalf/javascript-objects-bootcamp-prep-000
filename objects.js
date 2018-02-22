bbb01d2267e699239a3ae39d0218e80838754162
yossi katzman <issoyk@gmail.com>
2018-02-22 13:12:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 } 
 
 function removeFromPlaylist(playlist, artistName) {
-   return Object.assign()
+   delete playlist[artistName]
+   return playlist
    
 }
